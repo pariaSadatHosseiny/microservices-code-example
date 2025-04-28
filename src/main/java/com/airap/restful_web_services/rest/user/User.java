18a3e070d44957f0d3b5f8c01e2c88454d1678b6
pari <p.hosseiny72@gmail.com>
2025-04-28 14:51:34 +0200

    Customize rest response by jackson

--- src/main/java/com/airap/restful_web_services/rest/user/User.java
+++ src/main/java/com/airap/restful_web_services/rest/user/User.java
@@ -31,29 +31,28 @@ public class User {
      * Dynamic filtering: This method demonstrates dynamic filtering using Jackson's `@JsonFilter`.
      * The only annotation used here is `@JsonFilter("FilterName")`, and the rest of the logic is
      * implemented in the endpoint.
-     *
-     * Sample code:
-     * <pre>
-     * {@code
-     * // field1, field2 are exposed using filtering
-     * @GetMapping("/filtering")
-     * public MappingJacksonValue retrieveSomeBean() {
-     *     SomeBean someBean = new SomeBean("value1", "value2", "value3");
-     *
-     *     // Define which fields to include in the response
-     *     SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
-     *
-     *     // Apply the filter to the bean
-     *     FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
-     *
-     *     // Map the bean and apply the filter
-     *     MappingJacksonValue mapping = new MappingJacksonValue(someBean);
-     *     mapping.setFilters(filters);
-     *
-     *     return mapping;
-     * }
-     * }
-     * </pre>
      */
+
+    //Sample code:
+
+    // field1, field2 are exposed using filtering
+     /* @GetMapping("/filtering")
+      public MappingJacksonValue retrieveSomeBean() {
+          SomeBean someBean = new SomeBean("value1", "value2", "value3");
+
+          // Define which fields to include in the response
+          SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+
+          // Apply the filter to the bean
+          FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+
+          // Map the bean and apply the filter
+          MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+          mapping.setFilters(filters);
+
+          return mapping;
+      }
+*/
+
 
 }
